e0270adbe28e20db1173834c5837e9fb726a54fd
prasad96922 <prasadthaduka@gmail.com>
2026-05-22 10:48:45 +0530

    Module1IntroductionApplication-part1

---
+++ src/main/java/com/Module1Introduction/Module1IntroductionLvp/SpringBoot/Application.java
@@ -0,0 +1,13 @@
+package com.Module1Introduction.Module1IntroductionLvp.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
