70a12df57880f775ec8bba8cd3cd34aa3d9dd393
prasad96922 <prasadthaduka@gmail.com>
2026-05-22 12:15:06 +0530

    Part-1, Beans Explained

---
+++ src/test/java/com/Module1Introduction/Module1IntroductionLvp/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Module1Introduction.Module1IntroductionLvp.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
